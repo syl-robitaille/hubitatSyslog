3829ebf1b6444c086ec9a293a6b0c98d23e05054
Syl Robitaille <syl-robitaille@users.noreply.github.com>
2025-07-25 17:51:47 -0300

    Upload updated version (see extended description)
    - Incorporate rmonk's increased logging delay
    - "hostname" input field defaults to the hub's internal name
    - syslog facility is configurable / priority is derived from hub's log "level"
    - rfc5424 syslog protocol version is identified as such, rather than being just a magical "1"
    - jtp10181's escapeStringHTMLforMsg() method is applied to the incoming log message
    - we differentiate between "apps" and "devs" in the logged messages
    - loop avoidance checks that the incoming message wasn't from a "dev" in addition to checking that its id number doesn't match our own. DO NOT "logEnable" MORE THAN ONE "Syslog" DEVICE AT A TIME, OR YOU WILL GET A RECIPROCAL LOOP BETWEEN THEM.

--- drivers/Syslog.groovy
+++ drivers/Syslog.groovy
@@ -23,10 +23,27 @@
   - Attemping RFC5424 format for syslog
   - Date/time stamping with the hub timezone
 
+2024-01-29 - rmonk
+  - Changed the logging init delay to 10 seconds to try and get it
+    to properly start on boot
+
+2024-12-10 - Sylvain Robitaille
+  - "hostname" input field defaults to the hub's internal name
+  - syslog facility is configurable / priority is derived from hub's
+    log "level"
+  - rfc5424 syslog protocol version is identified as such, rather
+    than being just a magical "1"
+  - jtp10181's escapeStringHTMLforMsg() method is applied to the
+    incoming log message
+2024-12-28 - Sylvain Robitaille
+  - we differentiate between "apps" and "devs" in the logged messages
+  - loop avoidance checks that the incoming message wasn't from a
+    "dev" in addition to checking that its id number doesn't match our own
+
 */
 
 metadata {
-    definition (name: "Syslog", namespace: "hubitatuser12", author: "Hubitat User 12") {
+    definition (name: "Syslog", namespace: "hubitatuser12", author: "Hubitat User 12, staylorx, jtp10181, rmonk, Sylvain Robitaille (TheRockgarden)") {
         capability "Initialize"
     }
     command "disconnect"
@@ -35,7 +52,8 @@ metadata {
         input("ip", "text", title: "Syslog IP Address", description: "ip address of the syslog server", required: true)
         input("port", "number", title: "Syslog IP Port", description: "syslog port", defaultValue: 514, required: true)
         input("udptcp", "enum", title: "UDP or TCP?", description: "", defaultValue: "UDP", options: ["UDP","TCP"])
-        input("hostname", "text", title: "Hub Hostname", description: "hostname of the hub; leave empty for IP address")
+        input("logFacility", "enum", title: "Log Facility", description: "", defaultValue: "local0", options: ["kern","user","mail","daemon","auth","syslog","lpr","news","uucp","cron","authpriv","ftp","ntp","security","console","local0","local1","local2","local3","local4","local5","local6","local7"])
+        input("hostname", "text", title: "Hub Hostname", description: "hostname of the hub; leave empty for IP address", defaultValue: location.hub["name"])
         input("logEnable", "bool", title: "Enable debug logging", description: "", defaultValue: false)
     }
 }
@@ -70,25 +88,18 @@ void parse(String description) {
     }
     
     def descData = new JsonSlurper().parseText(description)
+    logLevel = descData.level != null ? descData.level : "info"
+
+    // rfc5424 syslog protocol version
+    def syslogVersion = 1
+
     // don't log our own messages, we will get into a loop
-    if("${descData.id}" != "${device.id}") {
+    if(!(descData.type == "dev" && "${descData.id}" == "${device.id}")) {
         if(ip != null) {
             // facility = 1 (user), severity = 6 (informational)
             // facility * 8 + severity = 14
-            def priority
-            switch (descData.level) {
-                case "info":
-                    priority = 14
-                    break
-                case "warn":
-                    priority = 12
-                    break
-                case "error":
-                    priority = 11
-                    break
-                default:
-                    priority = 15
-            }
+            // see  also getFacilityMap() and getPriorityMap() below ...
+            def priority = getFacilityMap()[logFacility] + getPriorityMap()[logLevel]}
             
             // we get date-space-time but would like ISO8601
             if (logEnable) log.debug "timezone from hub is ${location.timeZone.toString()}"
@@ -99,9 +110,12 @@ void parse(String description) {
             def isoDate = date.format("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", location.timeZone)
             if (logEnable) log.debug "time we get = ${descData.time}; time we want ${isoDate}"
             
+            // jtp10181
+            def message = escapeStringHTMLforMsg(descData.msg)
+
             // made up PROCID or MSGID //TODO find PROCID and MSGID in the API?
-            def constructedString = "<${priority}>1 ${isoDate} ${hostname} Hubitat - - [sd_id_1@32473 device_name=\"${descData.name}\" device_id=\"${descData.id}\"] ${descData.msg}"
-            if (logEnable) log.debug "sending: ${constructedString}"
+            def constructedString = "<${priority}>${syslogVersion} ${isoDate} ${hostname} " + descData.type + " " + descData.id.toString() + " - - ${message}"
+            if (logEnable) log.debug "sending: ${constructedString} to ${udptcp}:${ip}:${port}"
             
             if (udptcp == 'UDP') {
               if (logEnable) log.debug "UDP selected"
@@ -138,7 +152,9 @@ void uninstalled() {
 
 void initialize() {
     if (logEnable) log.debug "initialize()"
-    runIn(5, "connect")
+    log.info "Starting log export to syslog"
+    runIn(10, "connect")
+
 }
 
 void webSocketStatus(String message) {
@@ -149,3 +165,72 @@ void webSocketStatus(String message) {
         runIn(5, connect)
     }
 }
+
+// ------------------------------------------------------------------
+// 2024-08-25 jtp10181
+private String escapeStringHTMLforMsg(String str) {
+    if (str) {
+        str = str.replaceAll("&amp;", "&")
+        str = str.replaceAll("&lt;", "<")
+        str = str.replaceAll("&gt;", ">")
+        str = str.replaceAll("&#027;", "'")
+        str = str.replaceAll("&#039;", "'") 
+        str = str.replaceAll("&apos;", "'")
+        str = str.replaceAll("&quot;", '"')
+               str = str.replaceAll("&nbsp;", " ")
+        //Strip HTML Span Tags
+        str = str.replaceAll(/<\/?span.*?>/, "")
+    }
+    return str
+}
+
+// ------------------------------------------------------------------
+// 2024-12-10 Sylvain Robitaille
+private getFacilityMap() {
+    [
+        "kern":       (0<<3),
+        "user":       (1<<3),
+        "mail":       (2<<3),
+        "daemon":     (3<<3),
+        "auth":       (4<<3),
+        "syslog":     (5<<3),
+        "lpr":        (6<<3),
+        "news":       (7<<3),
+        "uucp":       (8<<3),
+        "cron":       (9<<3),
+        "authpriv":  (10<<3),
+        "ftp":       (11<<3),
+        "ntp":       (12<<3),
+        "security":  (13<<3),
+        "console":   (14<<3),
+//      "unused":    (15<<3),  /* Clock/cron daemon (Solaris) */
+        "local0":    (16<<3),
+        "local1":    (17<<3),
+        "local2":    (18<<3),
+        "local3":    (19<<3),
+        "local4":    (20<<3),
+        "local5":    (21<<3),
+        "local6":    (22<<3),
+        "local7":    (23<<3),
+    ]
+}
+
+private getPriorityMap() {
+    [
+        "emergency": 0,
+        "emerg":     0,
+        "panic":     0,
+        "alert":     1,
+        "critical":  2,
+        "crit":      2,
+        "error":     3,
+        "err":       3,
+        "warning":   4,
+        "warn":      4,
+        "notice":    5,
+        "info":      6,
+        "debug":     7,
+        "trace":     7,
+    ]
+}
+
